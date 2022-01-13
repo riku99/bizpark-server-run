7be18736b37f4142fcc9dac7f218b26ff5b72662
riku <riku.suny@gmail.com>
2022-01-13 16:10:21 +0900

    update pickedNews

--- src/resolvers/queries/pickedNews.ts
+++ src/resolvers/queries/pickedNews.ts
@@ -1,5 +1,7 @@
 import { QueryResolvers } from "~/generated/graphql";
 import { ForbiddenError } from "apollo-server-express";
+import { findNewsWithRelayStyle } from "~/models/news";
+import { createNewsConnection } from "~/helpers/createNewsConnection";
 
 export const pickedNews: QueryResolvers["pickedNews"] = async (
   _,
@@ -13,4 +15,52 @@ export const pickedNews: QueryResolvers["pickedNews"] = async (
   const decodedAfter = after
     ? Number(Buffer.from(after, "base64").toString())
     : null;
+
+  const news = await findNewsWithRelayStyle({
+    where: {
+      picked: {
+        some: {
+          pickerId: requestUser.id,
+        },
+      },
+    },
+    first,
+    after: decodedAfter,
+    userId: requestUser.id,
+  });
+
+  let count: number;
+  if (decodedAfter) {
+    count = await prisma.news.count({
+      where: {
+        picked: {
+          some: {
+            pickerId: requestUser.id,
+          },
+        },
+        cursor: {
+          lt: decodedAfter,
+        },
+      },
+    });
+  } else {
+    count = await prisma.news.count({
+      where: {
+        picked: {
+          some: {
+            pickerId: requestUser.id,
+          },
+        },
+      },
+    });
+  }
+
+  const connection = createNewsConnection({
+    news,
+    first,
+    after: decodedAfter,
+    count,
+  });
+
+  return connection;
 };
