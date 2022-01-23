cd0d5113db6ea226b4eb15073bb00fa3bbf5ffb3
riku <riku.suny@gmail.com>
2022-01-23 18:42:17 +0900

    fix order

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
+  // log: [
+  //   {
+  //     emit: "event",
+  //     level: "query",
+  //   },
+  // ],
 });
 
-prisma.$on("query", (e) => {
-  console.log("Query: " + e.query);
-  console.log("Duration: " + e.duration + "ms");
-});
+// prisma.$on("query", (e) => {
+//   console.log("Query: " + e.query);
+//   console.log("Duration: " + e.duration + "ms");
+// });
 
 export type Prisma = typeof prisma;
