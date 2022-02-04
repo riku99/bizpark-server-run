57a3cdd382932b217b1c0f4c79c2c5b8a341474e
riku <riku.suny@gmail.com>
2022-02-04 15:04:17 +0900

    add talkRoom

--- src/resolvers/NewsTalkRoomMessage/index.ts
+++ src/resolvers/NewsTalkRoomMessage/index.ts
@@ -1,8 +1,10 @@
 import { NewsTalkRoomMessageResolvers } from "~/generated/graphql";
 import { replyMessage } from "./replyMessage";
 import { sender } from "./sender";
+import { talkRoom } from "./talkRoom";
 
 export const NewsTalkRoomMessage: NewsTalkRoomMessageResolvers = {
   replyMessage,
   sender,
+  talkRoom,
 };
