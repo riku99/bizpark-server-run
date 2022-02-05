1502de9781dd894da5414ce75f47ebbad8e63f7c
riku <riku.suny@gmail.com>
2022-02-05 21:51:53 +0900

    fix subscription

--- src/resolvers/subscriptoins/newsTalkRoomMessageCreated.ts
+++ src/resolvers/subscriptoins/newsTalkRoomMessageCreated.ts
@@ -1,8 +1,39 @@
 // @ts-nocheck
 
-import { SubscriptionResolvers } from "~/generated/graphql";
+import {
+  SubscriptionResolvers,
+  SubscriptionNewsTalkRoomMessageCreatedArgs,
+} from "~/generated/graphql";
 import { pubsub } from "~/lib/pubsub";
+import { withFilter } from "graphql-subscriptions";
+import { prisma } from "~/lib/prisma";
+import { NewsTalkRoomMessage } from "@prisma/client";
 
 export const newsTalkRoomMessageCreated: SubscriptionResolvers["newsTalkRoomMessageCreated"] = {
-  subscribe: () => pubsub.asyncIterator(["NEWS_TALK_ROOM_MESSAGE_CREATED"]),
+  subscribe: withFilter(
+    () => pubsub.asyncIterator(["NEWS_TALK_ROOM_MESSAGE_CREATED"]),
+    async (
+      payload: { newsTalkRoomMessageCreated: NewsTalkRoomMessage },
+      variables: SubscriptionNewsTalkRoomMessageCreatedArgs
+    ) => {
+      const member = await prisma.newsTalkRoomMember.findUnique({
+        where: {
+          talkRoomId_userId: {
+            talkRoomId: payload.newsTalkRoomMessageCreated.roomId,
+            userId: variables.userId,
+          },
+        },
+      });
+
+      if (!member) {
+        return false;
+      }
+
+      const send = variables.roomIds.some(
+        (id) => id === payload.newsTalkRoomMessageCreated.roomId
+      );
+
+      return send;
+    }
+  ),
 };
