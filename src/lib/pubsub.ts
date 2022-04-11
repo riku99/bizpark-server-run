e5957a0ec3491e50a57ea08907ca44572baf27b6
riku <riku.suny@gmail.com>
2022-04-11 19:51:43 +0900

    add google pubsub

--- src/lib/pubsub.ts
+++ src/lib/pubsub.ts
@@ -1,3 +1,12 @@
-import { PubSub } from "graphql-subscriptions";
+import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { GooglePubSub } from '@axelspringer/graphql-google-pubsub';
+import Redis from 'ioredis';
 
-export const pubsub = new PubSub();
+const options = {
+  host: 'host.docker.internal',
+  port: 6379,
+};
+
+export const pubsub =
+  process.env.NODE_ENV === 'development' ? new PubSub() : new GooglePubSub();
