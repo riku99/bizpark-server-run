bd472732e5c0cef303243cb7ac97d89dad3a2d77
riku <riku.suny@gmail.com>
2022-04-13 23:19:26 +0900

    fix pg user

--- src/lib/pubsub.ts
+++ src/lib/pubsub.ts
@@ -21,8 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
   pubsub = new PostgresPubSub({ client }) as PubSub;
 } else {
   const client = new Client({
-    // user: process.env.DATABASE_USER,
-    user: 'dummyUser',
+    user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
