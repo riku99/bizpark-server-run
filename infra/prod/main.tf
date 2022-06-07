6a535df999a1aa68f6a3345070bbd606f3d05d7a
riku <riku.suny@gmail.com>
2022-06-07 22:39:06 +0900

    add cloud build to prod

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -18,6 +18,7 @@ provider "google" {
 locals {
   db_tier      = "db-custom-1-3840"
   db_disk_size = "10"
+  deploy_target_branch = "main"
 }
 
 module "cloud-sql" {
@@ -39,4 +40,14 @@ module "cloud-storage" {
   source = "../modules/cloud-storage"
   force_destroy = false
   name = "bizpark-user-upload"
+}
+
+module "cloud-build" {
+  source                      = "../modules/cloud-build"
+  region                      = var.region
+  cloudsql_instance_full_name = module.cloud-sql.bizpark-db-connection-name
+  registory_name              = var.registory_name # localにできそう(?)
+  bucket_name                 = module.cloud-storage.bucket_name
+  target_branch               = local.deploy_target_branch
+  app_bundle_id               = var.app_bundle_id
 }
